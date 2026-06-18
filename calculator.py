b2aab278138325bc15407c56b27784c96b8fa7ae
Adhithi Reddy <adhithipippera@gmail.com>
2026-06-18 21:21:55 +0530

    Added modulus operation

--- calculator.py
+++ calculator.py
@@ -1,7 +1,34 @@
-num1 = int(input("Enter first number:"))
-num2 = int(input("Enter second number:"))
+while True:
+    print("\n==== Calculator ====")
+    print("1. Addition")
+    print("2. Subtraction")
+    print("3. Multiplication")
+    print("4. division")
+    print("5. Modulus")
+    print("6. Exit")
 
-print("Addition = ",num1 + num2)
-print("Subtraction = ",num1 - num2)
-print("Multiplication = ",num1 * num2)
-print("Division = ",num1 / num2)
+
+    choice = input("Enter your choice: ")
+    print("Goodbye!")
+    break
+    
+    num1 = float(input("Enter first number:"))
+    num2 = float(input("Enter Second number"))
+
+    if choice == "1":
+        print("Result =", num1 + num2)
+
+    elif choice == "2":
+        print("Result =", num1 - num2)
+
+    elif choice =="3":
+        print("Result =", num1 * num2)
+
+    elif choice =="4":
+        print("Result =", num1 / num2)
+    
+    elif choice =="5":
+        print("Result =", num1 % num2)
+        
+    else:
+        print("Invalid Choice")
